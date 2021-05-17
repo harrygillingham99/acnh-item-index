370a441d916a2a296d7599200cb1b51dbf3701e3
harrygillingham99 <harrygillingham99@gmail.com>
2021-05-17 01:48:59 +0100

    fix: core.ignorecase false

---
+++ src/components/cards/SeaCreatureCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import { SeaCreature } from "../../types/AHCN-API/SeaCreature";
+
+export const SeaCreatureCard = (seaCreature: SeaCreature) => (
+  <Card className="text-center" key={seaCreature.id}>
+    <Card.Title>
+      {seaCreature.name["name-EUen"]}{" "}
+      <img alt="Fish Icon" className="icon ml-2" src={seaCreature.icon_uri} />
+    </Card.Title>
+    <div className="text-center ml-auto mr-auto">
+      <Card.Img
+        alt="Fish Photo"
+        style={{ height: 200, width: 400 }}
+        variant="top"
+        src={seaCreature.image_uri}
+      />
+    </div>
+    <ListGroup className=" mt-2 list-group-flush">
+      <ListGroupItem>
+        Museum Phrase: {seaCreature["museum-phrase"]}
+      </ListGroupItem>
+      <ListGroupItem>Price: {seaCreature.price}</ListGroupItem>
+      <ListGroupItem>Speed: {seaCreature.speed}</ListGroupItem>
+      <ListGroupItem>Catch Phrase: {seaCreature["catch-phrase"]}</ListGroupItem>
+      <ListGroupItem>
+        Availability: {JSON.stringify(seaCreature.availability)}
+      </ListGroupItem>
+    </ListGroup>
+  </Card>
+);
